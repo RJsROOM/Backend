d182b81535886d9575c10fdee0e11e53439fa9b4
Rakshit jha <rakshitjha05@gmail.com>
2026-01-27 18:06:12 +0530

    first commit

---
+++ Day1/index.js
@@ -0,0 +1,10 @@
+// packages are a set of pre-written codes which a developer makes and publishes it in public so that other developers can use it in their projects. These packages help to save time and effort as developers do not have to write code from scratch for common functionalities. They can simply install the package and use it in their application.
+// const catme= require('cat-me');
+// console.log(catme());
+// packagelock.json file stores that whatever code we have written on which it depends and further the package which we have used if that also is dependant on any other package then it is also mentioned in the packagelock.json and all the code files goes in the node module folder.
+// expresss is also a package which lets us create servers seamlesssly and is very easy to use.
+// const express= require('express');
+// const app=express() //server create ho chuka hai
+// app.listen(3000)  //server start ho chuka hai
+
+// .gitignore file is used to ignore certain files or folders from being tracked by git version control system. For example, node_modules folder is usually ignored as it contains all the packages which can be installed using package.json file and it can take up a lot of space in the repository.
