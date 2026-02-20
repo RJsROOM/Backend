086963955841c8e854da3fffd806d0796cd1d984
RAKSHIT JHA <rakshitjha05@gmail.com>
2026-02-20 16:59:41 +0530

    Day17-InstaG clone proj[5]

---
+++ Day17/src/config/database.js
@@ -0,0 +1,9 @@
+//iska kaam: database me operations control krna
+const mongoose=require("mongoose")
+
+async function connectToDb(){
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log("connected to MongoDB database")
+}
+
+module.exports=connectToDb;
